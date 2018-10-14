4697655ecb6916c5df757c865c50cf35d89e1638
Dave Joe M <davejoem@live.com>
2018-10-13 21:53:27 -0500

    Routes now working about to start integrating click actions and display

---
+++ dist/routes/lenders.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});class s{constructor(s){this.routes=[],this.mlipia=s,this.register(),this.listen()}listen(){this.routes.forEach(s=>{this.mlipia.app[s.method](s.address,s.func)})}list(s,e){return new Array}register(){this.routes.push({address:"/lenders/list",func:(s,e)=>{this.list(s,e)},method:"get"})}}exports.LendersRoutes=s;
